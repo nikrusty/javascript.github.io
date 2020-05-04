c3f588b6b66c3440c27a3006ed8e473dd16f119e
nikrusty <62635995+nikrusty@users.noreply.github.com>
2020-05-04 15:59:47 -0500

    updated functions

--- js/functions.js
+++ js/functions.js
@@ -1,21 +1,19 @@
 /*eslint-env browser*/
-
-window.addEventListener('load', saveInput, false);
-
-
-
+window.addEventListener('Submit', saveInput, false);
 
 function saveInput() {
 var orgName = document.getElementsByName("organizationName")[0].value;
 var date = document.getElementsByName("eventDate")[0].value;
 var website = document.getElementsByName("websiteURL")[0].value;
 var host = document.getElementsByName("hostName")[0].value;
-var name = document.getElementsByName("recipientName")[0].value;
+var recipient = document.getElementsByName("recipientName")[0].value;
 
-
-document.getElementById("recipientName").textContent = name;
-document.getElementById("organizationName").textContent = orgName;
+document.getElementById("recipientName").textContent = recipient;
+document.getElementById("organizationName").innerHTML = orgName;
 document.getElementById("eventDate").textContent = date;
 document.getElementById("websiteURL").textContent = website;
 document.getElementById("hostName").textContent = host;
+
+return false;
 }
+
